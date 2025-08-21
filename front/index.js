138e2f90c48f80e32798faaa5116354bbd6d24a6
Santiagolemosr <Santyagolemosr@gmail.com>
2025-08-21 11:30:05 -0500

    quinta version

--- front/index.js
+++ front/index.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://localhost:3000/api/equipos';
+const API_URL = 'http://localhost:3000/api/equipos';
 
 async function obtenerEquipos() {
     const res = await fetch(API_URL);
@@ -6,29 +6,25 @@ async function obtenerEquipos() {
     return equipos;
 }
 
-async function crearEquipos(data) {
+async function crearEquipo(data) {
     const res = await fetch(API_URL,{
         method: 'POST',
-        headers :{
-            'content-type': 'application/json'
-        },
+        headers :{'content-type': 'application/json' },
         body: JSON.stringify(data)
     });
     return await res.json();
 }
 
-async function actualizarEquipos(id,data) {
+async function actualizarEquipo(id,data) {
     const res = await fetch(`${API_URL}/${id}`,{
         method: 'PUT',
-        headers :{
-            'content-type': 'application/json'
-        },
+        headers :{'content-type': 'application/json' },
         body: JSON.stringify(data)
     });
     return await res.json();
 }
 
-async function eliminarEquipos(id) {
+async function eliminarEquipo(id) {
     const res = await fetch(`${API_URL}/${id}`,{
         method: 'DELETE',
     });
@@ -37,7 +33,8 @@ async function eliminarEquipos(id) {
 
 
 //REFERENCIAS A LOS ELEMENTOS DEL DOM
-const contenedorCards = document.getElementBy('contenedorCards');
+const id_equipo = document.getElementById('id_equipo');
+const contenedorCards = document.getElementById('contenedorCards');
 const templateCard  = document.getElementById('templateCard');
 const datoForm  = document.getElementById('datoForm');
 const nombre  = document.getElementById('nombre');
@@ -48,44 +45,47 @@ async function mostrarEquipos() {
     contenedorCards.innerHTML = '';
     const equipos = await obtenerEquipos();
     equipos.forEach(equipo => {
-        const clone = templateCard.contentEditable.cloneNode(true);
-        clone.queryselector('.nombreEquipos').textcontent = equipo.nombre_equipo;
-        clone.queryselector('.btn-editar').onclick = () => cargarEquipoparaEditar(equipo);
-        clone.queryselector('.btn-eliminar').onclick = () => eliminarEquipohandler(equipo.id_equipo);
-        contenedorCards.appendchild(clone);
-    })
+        const clone = templateCard.content.cloneNode(true);
+        clone.querySelector('.nombreEquipos').textContent = equipo.nombre_equipo;
+        clone.querySelector('.btn-editar').onclick = () => cargarEquipoParaEditar(equipo);
+        clone.querySelector('.btn-eliminar').onclick = () => eliminarEquipoHandler(equipo.id_equipo);
+        contenedorCards.appendChild(clone);
+    });
     
 }
 
+// GUARDAR O ACTUALIZAR EQUIPO
 datoForm.onsubmit = async(e) =>{
     e.preventDefault();
     const data = { nombre_equipo: nombre.value};
     if(id_equipo.value){
-        await actualizarEquipos(id_equipo.value,data);
+        await actualizarEquipo(id_equipo.value,data);
     } else{
-        await crearEquipos(data);
+        await crearEquipo(data);
     }
-    datoForm.requestFullscreen();
+    datoForm.reset();
     id_equipo.value='';
     await mostrarEquipos();
-}
+};
 
 //cancelar edicion
 btnCancelar.onclick =() => {
-    datoForm.res();
+    datoForm.reset();
     id_equipo.value = '';
-}
+};
 
 //CARGAR EQUIPO PARA EDITAR
-function cargarEquipoparaEditar(equipo) {
+function cargarEquipoParaEditar(equipo) {
     id_equipo.value = equipo.id_equipo;
     nombre.value = equipo.nombre_equipo;
 }
 
-async function eliminarEquipohandler(id) {
-    if (confirm('¿esta seguro de eliminar este equipo?')){
-        await eliminarEquipos(id)
+//ELIMINAR EQUIPO
+
+async function eliminarEquipoHandler(id) {
+     await eliminarEquipo(id);
+     mostrarEquipos();
     }
     
-}
+
 mostrarEquipos();
